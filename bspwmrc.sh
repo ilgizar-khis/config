2c481ba139093792891ad08675a28cfd2054793c
razigli <razigli@ufa.dev>
2026-02-27 20:52:33 +0500

    Fri Feb 27 08:52:33 PM +05 2026

--- bspwmrc.sh
+++ bspwmrc.sh
@@ -6,6 +6,9 @@ picom &
 xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --output eDP-1 --mode 1920x1080 --pos 0x0 &
 feh --bg-scale /home/razigli/Pictures/.bg/{6.jpeg,33.png} &
 setxkbmap -layout "us,ru" -option "grp:win_space_toggle" &
-bspc rule -a * title="TLauncher" state=floating &
+xset s off &
+xset s noblank &
+xset -dmps &
+# bspc rule -a * title="TLauncher" state=floating &
 bspc monitor HDMI-1 -d  "6" "7" "8" "9" "0"
 bspc monitor eDP-1 -d "1" "2" "3" "4" "5"
