b95750fa57ceba528813e012c0e49bf6bd4ee2f8
razigli <razigli@ufa.dev>
2026-03-07 23:37:20 +0500

    add are niri Sat Mar  7 11:37:20 PM +05 2026

--- bspwmrc.sh
+++ bspwmrc.sh
@@ -5,10 +5,11 @@ sxhkd &
 picom &
 xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --output eDP-1 --mode 1920x1080 --pos 0x0 &
 feh --bg-scale /home/razigli/Pictures/.bg/{6.jpeg,33.png} &
-setxkbmap -layout "us,ru" -option "grp:win_space_toggle" &
-xset s off &
-xset s noblank &
-xset -dmps &
+# setxkbmap -layout "us,ru" -option "grp:win_space_toggle" &
+# xset s off &
+# xset s noblank &
+# xset -dmps &
+xset s off -dpms
 # bspc rule -a * title="TLauncher" state=floating &
 float_width="1400"
 float_height="800"
@@ -20,5 +21,6 @@ bspc rule -a '*:*:pacseek' state=floating rectangle=${float_width}x${float_heigh
 bspc rule -a '*:*:vifm' state=floating rectangle=${float_width}x${float_height}+${float_posx}+${float_posy} & 
 bspc rule -a '*:*:btop' state=floating rectangle=${float_width}x${float_height}+${float_posx}+${float_posy} & 
 bspc rule -a 'org-tlauncher-tlauncher-rmo-TLauncher' desktop=6 state=floating
+bspc rule -a 'hyp.exe' state=floating
 bspc monitor HDMI-1 -d  "6" "7" "8" "9" "0"
 bspc monitor eDP-1 -d "1" "2" "3" "4" "5"
--- polybar/window.sh
+++ polybar/window.sh
@@ -4,7 +4,7 @@ window_id=$(bspc query -N -n .active.window -m $1)
 
 if [ -n "$window_id" ]; then
     window=$(xdotool getwindowname $window_id)
-    echo "${window:0:100}"
+    echo "${window:0:50}"
 else
     echo "bspwm"
 fi
