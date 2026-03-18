a3aa885377157277a2ad50b2e4dbebc42b2c23c1
razigli <razigli@arch.dev>
2026-03-18 19:37:12 +0500

    before test md

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,40 @@
+return {
+  -- Markdown support with treesitter and preview
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    ft = "markdown",
+    opts = {
+      preview = {
+        enabled = true,
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+
+  -- Grip for GitHub-style preview
+  {
+    "rhysd/vim-grip",
+    ft = "markdown",
+  },
+
+  -- Better markdown editing experience
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
+
+  -- Treesitter markdown parser
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "markdown", "markdown_inline" },
+    },
+  },
+}
