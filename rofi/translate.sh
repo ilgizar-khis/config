84c0e5769dee8b0ca715a5cc0f83a78a8242d2ba
Ваше Имя <you@example.com>
2026-03-28 16:25:53 +0500

    Сб 28 мар 2026 16:25:53 +05

--- rofi/translate.sh
+++ rofi/translate.sh
@@ -6,7 +6,7 @@ if [[ "$ROFI_RETV" == "2" ]]; then
     text="$(echo "$1" | cut -d' ' -f2-)"
     # echo "$lang"
     # echo "$text"
-    echo "$(trans -no-ansi :$lang "$text")"
+    echo "$(trans -no-ansi $lang "$text")"
 elif [[ "$ROFI_RETV" == "1" ]]; then
     wl-copy $1
 fi
