3e90b820abccb5d4406fc5c46a6193c2b4ed3b68
razigli <razigli@arch.dev>
2026-07-17 20:51:17 +0500

    require lua.events

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -32,14 +32,10 @@ local status_bar = "noctalia"
 local dbus = "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
 
 -------------------
----- AUTOSTART ----
+---- EVENTS ----
 -------------------
 
-hl.on("hyprland.start", function()
-	hl.exec_cmd(status_bar)
-	hl.exec_cmd(dbus)
-	hl.exec_cmd("diskie")
-end)
+require("lua.events")
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
