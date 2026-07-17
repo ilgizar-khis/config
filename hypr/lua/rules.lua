7bc5fe4ed55df05a01393315dd8c07197a73ed33
razigli <razigli@arch.dev>
2026-07-17 20:44:41 +0500

    also add other rules

--- hypr/lua/rules.lua
+++ hypr/lua/rules.lua
@@ -56,3 +56,58 @@ hl.window_rule({
 	match = { class = "hyprland-share-picker" },
 	float = true,
 })
+
+-- Example window rules that are useful
+hl.window_rule({
+	name = "TLauncher",
+	match = { class = "org-tlauncher-tlauncher-rmo-TLauncher" },
+	float = true,
+	center = true,
+})
+
+hl.window_rule({
+	name = "nwg-look",
+	match = { class = "nwg-look" },
+	float = true,
+})
+
+local suppressMaximizeRule = hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+
+	suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
+
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
+
+	no_focus = true,
+})
+
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
+
+	move = "20 monitor_h-120",
+	float = true,
+})
