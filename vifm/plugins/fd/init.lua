82e74361de56b32d4956bf8cf7b22f0e0b5bda67
razigli <razigli@arch.dev>
2026-08-10 02:31:14 +0500

    start developing of completion

--- vifm/plugins/fd/init.lua
+++ vifm/plugins/fd/init.lua
@@ -3,6 +3,11 @@ local M = {}
 local function to_cmd(tbl)
 	local cmd = ""
 	for _, s in ipairs(tbl) do
+		if string.find(s, "^%~") then
+			s = string.gsub(s, "^%~", tostring(os.getenv("HOME")))
+		elseif string.find(s, "^%.") then
+			s = string.gsub(s, "^%.", tostring(os.getenv("PWD")))
+		end
 		cmd = cmd .. '"' .. tostring(s) .. '" '
 	end
 	return cmd
@@ -34,6 +39,31 @@ local function main(opt)
 	end
 end
 
-vifm.cmds.add({ name = "fd", maxargs = 3, handler = main, description = "use fd, not find" })
+local function complete(opt)
+	local view = vifm.currview()
+	if string.find(opt.arg, "^%-") then
+		local line = ""
+		for i = 1, #opt.argv - 1 do
+			line = line .. opt.argv[i] .. " "
+		end
+		line = line .. "--hidden"
+		return { matches = { line } }
+	end
+	if #opt.argv > 1 then
+		local files = {}
+		local line = ""
+		for i = 1, #opt.argv - 1 do
+			line = line .. opt.argv[i] .. " "
+		end
+		for entry in view:entries() do
+			if string.find(entry.name, "^" .. opt.arg) then
+				table.insert(files, line .. " " .. tostring(os.getenv("PWD")) .. "/" .. entry.name)
+			end
+		end
+		return { matches = files }
+	end
+end
+
+vifm.cmds.add({ name = "fd", maxargs = 3, handler = main, description = "use fd, not find", complete = complete })
 
 return M
